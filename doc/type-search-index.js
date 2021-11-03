f5ccdcc388b5afa2ddce03330871f68390f6d2d2
Cristhian.cfg <cristhianfg.cfg@gmail.com>
2021-11-03 11:18:04 +0100

    Javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"dades","l":"LlistaEstacionsRecarrega"}];updateSearchResults();
--- src/dades/LlistaEstacionsRecarrega.java
+++ src/dades/LlistaEstacionsRecarrega.java
@@ -1,15 +1,31 @@
 package dades;
 
+/**
+ * @author Cristhian Fajardo
+ * @author Arnau Papiol
+ */
 public class LlistaEstacionsRecarrega {
 	private EstacioRecarregaVE[] lista;
 	private int numEstacions;
 	private static double esPropera = 30; 
 
+	/**
+	 * Constructor lista vacia y 0 estaciones
+	 */
 	public LlistaEstacionsRecarrega() {
 		this.lista = new EstacioRecarregaVE[0];
 		this.numEstacions = 0;
 	}
 
+	/**
+	 * @param EstacioRecarregaVE e
+	 * 
+	 * Añade una estación al final pero si no hay espacio en la lista
+	 * redimensiona esta de manera rudimentaria, creamos una nueva lista de 2
+	 * mas de longitud, se rellena con los datos que tenemos y esta pasa a ser
+	 * la lista principal
+	 * 
+	 */
 	public void afegirEstacions(EstacioRecarregaVE e) {
 		if (numEstacions >= this.lista.length) {
 			EstacioRecarregaVE[] listaCopia = new EstacioRecarregaVE[numEstacions + 2];
@@ -22,9 +38,17 @@ public class LlistaEstacionsRecarrega {
 		numEstacions++;
 	}
 
+	/**
+	 * @param String poblacion
+	 * 
+	 * Elimina todas las estaciones que sean de ese municipio, nunca dejará un
+	 * espacio vacio en la lista con lo que desplazara hacia la izquierda todos
+	 * los elementos cada vez que se encuentre una estación a eliminar
+	 * 
+	 */
 	public void eliminarConjuntEstacions(String pob) {
 		for (int i = 0; i < numEstacions; i++) {
-			if (lista[i].esTrobaEnAquestaProvincia(pob) || lista[i].esTrobaEnAquestMunicipi(pob)) {
+			if (lista[i].esTrobaEnAquestMunicipi(pob)) {
 				for (int j = i; j <= numEstacions-2; j++) {
 					lista[j] = lista[j + 1];
 				}
@@ -34,21 +58,29 @@ public class LlistaEstacionsRecarrega {
 
 	}
 
+	/**
+	 * @param String poblacion
+	 * @return un print con los datos de todas las estaciones que esten en esa población
+	 */
 	public String toStringPoblacio(String pob) {
 		String aux = "";
 		for (int i = 0; i < numEstacions; i++) {
-			if (lista[i].esTrobaEnAquestaProvincia(pob) || lista[i].esTrobaEnAquestMunicipi(pob))
+			if (lista[i].esTrobaEnAquestMunicipi(pob))
 				aux = aux + lista[i] + "\n";
 		}
 		return aux;
 	}
 
+	/**
+	 * @param String poblacion
+	 * @return primera instancia de estación que encuentre en ese municipio
+	 */
 	public EstacioRecarregaVE Instancia_PrimeraEstacion(String pob) {
 		EstacioRecarregaVE aux = null;
 		int i = 0;
 		boolean trobat = false;
 		while (i < numEstacions && !trobat) {
-			if (lista[i].esTrobaEnAquestaProvincia(pob) || lista[i].esTrobaEnAquestMunicipi(pob)) {
+			if (lista[i].esTrobaEnAquestMunicipi(pob)) {
 				aux = lista[i];
 				trobat = true;
 			}
@@ -67,6 +99,11 @@ public class LlistaEstacionsRecarrega {
 	
 	// Metodo 7 retornar un duplicat de la instancia de l'estacio mes propera a la
 	// nostra posicio (Metodo distanciaA de la clase EstacioRecarregaE)
+	/**
+	 * @param latitud
+	 * @param longitud
+	 * @return instancia de l'estació mes propera
+	 */
 	public EstacioRecarregaVE EstacioMesPropera(int lat, int longi) {
 		EstacioRecarregaVE e = lista[0].copia();
 		//Ya guardamos la primera distancia para tener una con la que comparar
@@ -82,6 +119,13 @@ public class LlistaEstacionsRecarrega {
 		return e;
 	}
 	
+	/**
+	 * @param double proper
+	 * 
+	 * Modifica la variable static de la clase, que ens diu la distancia per dir
+	 * que la estació esta a prop nostra.
+	 * 
+	 */
 	public static void setEsPropera(double p) {
 		esPropera = p;
 	}
